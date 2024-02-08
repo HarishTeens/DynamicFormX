85cbabd26f8fcc65d4933b569b39be8e7550b951
HarishTeens <arishh2@gmail.com>
2024-02-09 03:14:54 +0530

    man this layout is so difficult, im crying. thanks gpt

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import AceEditor from 'react-ace';
 
 import 'ace-builds/src-noconflict/mode-json';
@@ -7,14 +6,30 @@ import 'ace-builds/src-noconflict/theme-monokai';
 function App() {
   const jsonData = { "name": "John", "age": 30, "city": "New York" };
   return (
-    <AceEditor
-      mode="json"
-      theme="monokai"
-      value={JSON.stringify(jsonData, null, 4)}
-      onChange={newValue => console.log(newValue)}
-      name="UNIQUE_ID_OF_DIV"
-      editorProps={{ $blockScrolling: true }}
-    />
+    <div className="flex flex-col h-screen">
+      <h1 className='text-center text-3xl'>DynamicFormX</h1>
+      <div className="flex-grow border mt-5">
+        <div className="flex flex-row h-full">
+          <div className="w-1/2 border">
+            <AceEditor
+              mode="json"
+              theme="monokai"
+              value={JSON.stringify(jsonData, null, 4)}
+              onChange={newValue => console.log(newValue)}
+              name="UNIQUE_ID_OF_DIV"
+              editorProps={{ $blockScrolling: true }}
+              fontSize={14}
+              style={{ height: '100%', width: '100%' }}
+            />
+          </div>
+          <div className="w-1/2 border">
+            <h2 className="text-center text-2xl">Preview</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
   );
 }
 
