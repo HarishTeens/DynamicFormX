3245d0f4fa6cc02bc84637c7950babc77fb02ebb
HarishTeens <arishh2@gmail.com>
2024-09-23 19:21:56 +0530

    fix routing error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DynamicFormX/',
   optimizeDeps: {
     exclude: ['jsoneditor/dist/jsoneditor-minimalist', 'jsoneditor/dist/jsoneditor.css']
   },
